16d8529a7a12b51c193bc44198d0ba5d7604af53
ario@riz.ky <rizkyario@me.com>
2022-06-18 11:12:29 +0200

    Updates

--- _next/static/IM-mjzRGxAphvhoSKkSaX/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/399-83a17d2cff27598e.js","static/chunks/pages/index-b35bd0db310e4560.js"],"/_error":["static/chunks/pages/_error-0260c064403af775.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
