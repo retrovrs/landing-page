0dc9ff81f04a304dcad52b3662c07c0c4f3a78fa
rizky <rizky@users.noreply.github.com>
2022-06-19 15:25:37 +0000

    deploy: d2a9b18044f11aa19e500f0727f90d2decaa5a06

--- _next/static/Ys8TEdbu5vxYDkSGBHNnl/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/361-1246c972f1f8f0f3.js","static/chunks/pages/index-0d1fdb1bdde642bf.js"],"/_error":["static/chunks/pages/_error-0260c064403af775.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
