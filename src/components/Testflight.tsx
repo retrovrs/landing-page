be064bff55c0bf9448d3bc82c3cd446607b919c5
Rizky Ario <rizkyario@me.com>
2023-02-14 20:10:24 +0100

    Change testflight to Appstore

--- src/components/Testflight.tsx
+++ src/components/Testflight.tsx
@@ -7,16 +7,16 @@ const Testflight = () => {
   const { testflight } = config;
 
   return (
-    <section className={`bg-background py-8`} id="testflight">
+    <section className={`bg-background py-8`} id="appstore">
       <div className={`container max-w-5xl mx-auto m-8`}>
         <h1
           className={`w-full my-2 text-5xl font-bold leading-tight text-center`}
         >
-          <span>Testflight</span>
+          <span>Appstore</span>
         </h1>
         <Divider />
         <div className={`flex flex-wrap flex-col-reverse sm:flex-row`}>
-          <div className={`w-full sm:w-1/2 p-6 mt-20`}>
+          <div className={`w-full sm:w-1/2 p-6 mt-10`}>
             <div className={`align-middle`}>
               <h3
                 className={`text-3xl text-gray-800 font-bold leading-none mb-3`}
