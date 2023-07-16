a21aaf60f75115e50bd89dd16c2ca2a6cc5b9899
Rizky Ario <rizkyario@me.com>
2023-07-16 17:30:22 +0200

    Fix crash

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -2,18 +2,14 @@ import { FC } from 'react';
 
 import ParticleAnimation from 'react-particle-animation';
 
-import useResponsiveSize from '../hooks/useResponsiveSize';
-
 const Canvas: FC = () => {
-  const { width } = useResponsiveSize();
   return (
-    <div style={{ width, height: 220 }}>
+    <div style={{ width: '100%', height: 220 }}>
       <ParticleAnimation
         background={{ r: 15, g: 23, b: 42, a: 255 }}
         color={{ r: 123, g: 97, b: 225, a: 255 }}
-        numParticles={(width ?? 100) / 8}
+        numParticles={160}
         particleSpeed={0.5}
-        particleRadius={1}
       />
     </div>
   );
